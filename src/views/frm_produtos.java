028d5d43cee3b412eed4a5b52f100b82a716b8b8
BitNers <wesleyalvessilva@outlook.com.br>
2021-11-09 21:22:07 -0300

    Atualizando View de visualização de produtos

--- src/views/frm_produtos.java
+++ src/views/frm_produtos.java
@@ -5,6 +5,8 @@ import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
@@ -22,22 +24,35 @@ import javax.swing.JCheckBox;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFormattedTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.border.EtchedBorder;
 
 public class frm_produtos {
 
 	private JFrame frmProdutos;
 	private JTextField txtField_ProcuraNome;
-	private JTable table_result;
+	private JTable table_result ;
 	
 	private JComboBox comboBox_Marcas;
 	private JComboBox comboBox_Tipos;
+	
+	private JScrollPane scrollPane_Result;
+	
+	private JFormattedTextField txtField_PrecoMin;
+	private JFormattedTextField txtField_PrecoMax;
+	
+	private JCheckBox chckbx_considera_apenas_disponiveis;
+	
+	private JPanel panel_results;
 
 	public frm_produtos() {
 		
 		ProdutoDAO produtodao = new ProdutoDAO();
 		
 		try {
-		
+			table_result = produtodao.GetProdutos("");
 			ArrayList<String> marcas = produtodao.GetMarcasExistentes();
 			ArrayList<String> tipos = produtodao.GetTipoExistentes();
 			comboBox_Marcas = new JComboBox(marcas.toArray());
@@ -54,6 +69,7 @@ public class frm_produtos {
 
 	private void initialize() {
 		frmProdutos = new JFrame();
+		frmProdutos.setResizable(false);
 		frmProdutos.getContentPane().setBackground(new Color(139, 69, 19));
 		frmProdutos.addWindowListener(new WindowAdapter() {
 			@Override
@@ -72,6 +88,7 @@ public class frm_produtos {
 		frmProdutos.getContentPane().setLayout(null);
 		
 		JPanel panel_filters = new JPanel();
+		panel_filters.setBorder(new EtchedBorder(EtchedBorder.RAISED, Color.ORANGE, Color.PINK));
 		panel_filters.setBounds(10, 11, 864, 116);
 		frmProdutos.getContentPane().add(panel_filters);
 		panel_filters.setLayout(null);
@@ -106,11 +123,11 @@ public class frm_produtos {
 		lbl_pesquisaporprecomax.setBounds(405, 45, 113, 14);
 		panel_filters.add(lbl_pesquisaporprecomax);
 		
-		JCheckBox chckbx_considera_disponiveis = new JCheckBox("Considerar Indispon\u00EDveis?");
-		chckbx_considera_disponiveis.setVerticalAlignment(SwingConstants.BOTTOM);
-		chckbx_considera_disponiveis.setHorizontalAlignment(SwingConstants.TRAILING);
-		chckbx_considera_disponiveis.setBounds(563, 82, 196, 23);
-		panel_filters.add(chckbx_considera_disponiveis);
+		chckbx_considera_apenas_disponiveis = new JCheckBox("Considerar apenas Dispon\u00EDveis");
+		chckbx_considera_apenas_disponiveis.setVerticalAlignment(SwingConstants.BOTTOM);
+		chckbx_considera_apenas_disponiveis.setHorizontalAlignment(SwingConstants.TRAILING);
+		chckbx_considera_apenas_disponiveis.setBounds(522, 82, 237, 23);
+		panel_filters.add(chckbx_considera_apenas_disponiveis);
 		
 		JButton btn_applyFilter = new JButton("Procurar");
 		btn_applyFilter.setBounds(765, 64, 89, 41);
@@ -124,25 +141,90 @@ public class frm_produtos {
 		comboBox_Tipos.setBounds(149, 73, 219, 22);
 		panel_filters.add(comboBox_Tipos);
 		
-		JFormattedTextField txtField_PrecoMin = new JFormattedTextField();
+	    DecimalFormat amountFormat = new DecimalFormat("#.00");
+	    txtField_PrecoMin = new JFormattedTextField(amountFormat);
+		
 		txtField_PrecoMin.setBounds(522, 11, 89, 20);
 		panel_filters.add(txtField_PrecoMin);
 		
-		JFormattedTextField txtField_PrecoMax = new JFormattedTextField();
+		txtField_PrecoMax = new JFormattedTextField(amountFormat);
 		txtField_PrecoMax.setBounds(522, 42, 89, 20);
 		panel_filters.add(txtField_PrecoMax);
 		
-		JPanel panel_results = new JPanel();
+		JButton btn_adiciona_carrinho = new JButton("Adicionar ao Carrinho");
+		btn_adiciona_carrinho.setFont(new Font("Arial", Font.PLAIN, 14));
+		btn_adiciona_carrinho.setBounds(674, 11, 180, 41);
+		panel_filters.add(btn_adiciona_carrinho);
+		
+		panel_results = new JPanel();
+		panel_results.setBorder(new EtchedBorder(EtchedBorder.RAISED, Color.ORANGE, Color.PINK));
 		panel_results.setBounds(10, 138, 864, 412);
 		frmProdutos.getContentPane().add(panel_results);
 		panel_results.setLayout(null);
 		
-		JScrollPane scrollPane_Result = new JScrollPane();
-		scrollPane_Result.setBounds(0, 0, 1, 1);
+		
+		scrollPane_Result = new JScrollPane();
+		scrollPane_Result.setBounds(0, 0, panel_results.getSize().width, panel_results.getSize().height);
 		panel_results.add(scrollPane_Result);
 		
-		table_result = new JTable();
+		table_result.setAutoResizeMode(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS);
+		table_result.getTableHeader().setReorderingAllowed(false);
+		
+	
 		scrollPane_Result.setViewportView(table_result);
 		frmProdutos.setVisible(true);
+		
+		btn_applyFilter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				refreshResultadoFinal();
+			}
+		});
+		
+	}
+	
+	private void refreshResultadoFinal() {
+		boolean temPesquisa = (this.txtField_ProcuraNome.getText().equals("")) ? false : true;
+		boolean temMarca = (this.comboBox_Marcas.getSelectedItem().toString().equals("Todos")) ? false : true;
+		boolean temTipo = (this.comboBox_Tipos.getSelectedItem().toString().equals("Todos")) ? false : true;
+		boolean considerarApenasDisponivel = (this.chckbx_considera_apenas_disponiveis.isSelected()) ? true : false;
+		boolean temprecoMin = (this.txtField_PrecoMin.getText().equals(""))? false : true;
+		boolean temprecoMax = (this.txtField_PrecoMax.getText().equals(""))? false : true;
+		
+		StringBuffer parameters = new StringBuffer();
+		parameters.append(" WHERE (1=1) \n");
+		
+		if(temPesquisa)
+			parameters.append(" AND `descricao` LIKE '%"+this.txtField_ProcuraNome.getText()+"%'\n");
+		
+		if(temMarca)
+			parameters.append(" AND `marca` = '"+this.comboBox_Marcas.getSelectedItem().toString()+"'\n");
+		
+		if(temTipo)
+			parameters.append(" AND `tipo_produto` = '"+this.comboBox_Tipos.getSelectedItem().toString()+"'\n");
+		
+		if(considerarApenasDisponivel)
+			parameters.append(" AND `disponivel` = 1\n");
+		
+		if(temprecoMin)
+			parameters.append(" AND `valor` >= "+this.txtField_PrecoMin.getText()+"\n");
+		
+		if(temprecoMax)
+			parameters.append(" AND `valor` <= "+this.txtField_PrecoMax.getText()+"\n");
+		
+		try {
+						
+			table_result = new ProdutoDAO().GetProdutos(parameters.toString());
+			table_result.setAutoResizeMode(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS);
+			table_result.getTableHeader().setReorderingAllowed(false);
+			scrollPane_Result.setViewportView(table_result);
+			
+			table_result.revalidate();
+			table_result.repaint();
+			
+			//panel_results.repaint();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
