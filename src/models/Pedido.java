bf0a1dfaf552d0cdfb8da7da9e33d5f2a6bb8513
BitNers <wesleyalvessilva@outlook.com.br>
2021-11-11 21:58:11 -0300

    Criando novo modelo para Pedido

---
+++ src/models/Pedido.java
@@ -0,0 +1,20 @@
+ package models;
+
+import java.util.ArrayList;
+
+public class Pedido {
+
+	private String cpf_cliente;
+	private ArrayList<Produto> lista_produtos;
+	private double valor_final;
+	private String metodo_pagamento;
+	private int funcionarioid;
+	
+	
+	public Pedido() {}
+	
+	
+	
+	
+	
+}
