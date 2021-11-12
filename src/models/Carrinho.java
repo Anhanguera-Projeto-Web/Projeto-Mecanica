9f0a7bf6d54c8dc38b61d33ef13861af047ab791
BitNers <wesleyalvessilva@outlook.com.br>
2021-11-11 21:57:57 -0300

    Atualizando modelo Carrinho

--- src/models/Carrinho.java
+++ src/models/Carrinho.java
@@ -6,5 +6,18 @@ public class Carrinho {
 
 	private ArrayList<Produto> lista_de_produtos;
 	
-	public Carrinho() {}
+	public Carrinho() {lista_de_produtos = new ArrayList<Produto>();}
+
+	public ArrayList<Produto> getLista_de_produtos() {
+		return lista_de_produtos;
+	}
+
+	public void setLista_de_produtos(Produto prod) {
+		this.lista_de_produtos.add(prod);
+	}
+	
+	public void limparCarrinho() {
+		this.lista_de_produtos = new ArrayList<Produto>();
+	}
+	
 }
